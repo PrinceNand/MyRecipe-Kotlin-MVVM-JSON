1a47d5dbd5503a59c69e362c19e13d9eef096a09
Prince <ashnand2000@gmail.com>
2024-10-05 18:34:11 +0530

    Recipe: 1.Initial Sync

---
+++ app/src/main/java/com/example/myrecipe_kotlin_mvvm_json/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.myrecipe_kotlin_mvvm_json
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myrecipe_kotlin_mvvm_json.ui.theme.MyRecipeKotlinMVVMJSONTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyRecipeKotlinMVVMJSONTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Developers",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyRecipeKotlinMVVMJSONTheme {
+        Greeting("Android")
+    }
+}
