d0b5a033a950ea33c6559c8b0394002f9c9d5d81
Prince <ashnand2000@gmail.com>
2024-10-06 20:20:45 +0530

    Recipe: 11.Adding "kotlin-parcelize" to parcelize object data Category in Screen routes

--- app/src/main/java/com/example/myrecipe_kotlin_mvvm_json/Category.kt
+++ app/src/main/java/com/example/myrecipe_kotlin_mvvm_json/Category.kt
@@ -1,12 +1,16 @@
 package com.example.myrecipe_kotlin_mvvm_json
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 //API: www.themealdb.com/api/json/v1/1/categories.php
 
+@Parcelize
 data class Category (val idCategory: String,
     val strCategory: String,
     val strCategoryThumb: String,
-    val strCategoryDescription: String
-    )
+    val strCategoryDescription: String): Parcelable
+
 
 // get a list of individual category item i.e., Categories
 data class CategoriesResponse(val categories: List<Category>)
