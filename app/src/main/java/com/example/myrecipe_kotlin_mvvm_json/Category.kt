64cb0589e303fd54161f60b54ae01f5f41edc32d
Prince <ashnand2000@gmail.com>
2024-10-06 20:37:39 +0530

    Recipe: 14.Parcelize and parcelable explanation

--- app/src/main/java/com/example/myrecipe_kotlin_mvvm_json/Category.kt
+++ app/src/main/java/com/example/myrecipe_kotlin_mvvm_json/Category.kt
@@ -14,3 +14,24 @@ data class Category (val idCategory: String,
 
 // get a list of individual category item i.e., Categories
 data class CategoriesResponse(val categories: List<Category>)
+
+/*
+Parcelize is a Kotlin feature that simplifies the implementation of the Parcelable interface,
+which allows objects to be serialized and passed between Android components like activities and
+fragments. By using the @Parcelize annotation on data classes, developers can automatically
+generate the necessary serialization code, reducing boilerplate and improving code readability.
+This approach enhances performance compared to Java's Serializable and leverages Kotlin’s type safety,
+making it easier to manage complex data structures
+while ensuring efficient data transfer in Android applications.*/
+
+/*Ex:
+plugins {
+    id 'kotlin-android'
+    id 'kotlin-parcelize'
+}
+@Parcelize
+data class User(
+    val id: Int,
+    val name: String,
+    val email: String
+) : Parcelable*/
